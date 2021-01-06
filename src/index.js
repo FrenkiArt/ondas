d9abc2427ed4a281c56136ba6b36640c8ceec3d5
frenkiart <frenki_art@mail.ru>
2021-01-06 21:25:15 +0300

    Сделана ещё 1 секция .ecosystem

--- src/index.js
+++ src/index.js
@@ -13,9 +13,28 @@ import './views/modules/footer/footer.scss';
 import './views/modules/social-links/social-links.scss';
 import './views/modules/team/team.scss';
 import './views/modules/roadmap/roadmap.scss';
+import './views/modules/ecosystem/ecosystem.scss';
 
 window.onresize = () => {
   setTimeout(() => {
     console.log(window.innerWidth);
   }, 1);
 };
+
+if (document.querySelector('.link-read-more')) {
+  document.querySelectorAll('.link-read-more').forEach((link) => {
+    link.addEventListener('click', clickLinkReadMoreHandler);
+  });
+}
+
+/**
+ * This function make visible hidden content in .link-read-more__target,
+ * after click on .link-read-more trigger.
+ * And hide trigger .link-read-more trigger
+ * @param {e} e - is event click.
+ * @return {void}
+ */
+function clickLinkReadMoreHandler(e) {
+  e.preventDefault();
+  e.target.closest('.link-read-more__wrapper').classList.add('open');
+}

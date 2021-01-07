17d09bdf2f48db015d2d0d376d6a4cb04ade2a10
frenkiart <frenki_art@mail.ru>
2021-01-07 23:58:47 +0300

    made: section for investors

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ import './views/modules/ecosystem/ecosystem.scss';
 import './views/modules/for-developers/for-developers.scss';
 import './views/modules/about/about.scss';
 import './views/modules/for-players/for-players.scss';
+import './views/modules/for-investors/for-investors.scss';
 
 window.onresize = () => {
   setTimeout(() => {
@@ -40,4 +41,80 @@ if (document.querySelector('.link-read-more')) {
 function clickLinkReadMoreHandler(e) {
   e.preventDefault();
   e.target.closest('.link-read-more__wrapper').classList.add('open');
+}
+
+if (document.querySelector('.invest-cart__pagination')) {
+  document
+    .querySelectorAll('.invest-cart__pagination .bullet')
+    .forEach((bullet) => {
+      bullet.addEventListener('click', clickPaginationBulletHandler);
+    });
+
+  autoTogglerForIvestCarts();
+}
+
+/**
+ * Pagination toggler
+ * @param {e} e - is event click.
+ * @return {void}
+ */
+function clickPaginationBulletHandler(e) {
+  const bulletTarget = e.target.closest('.bullet');
+  const allBullets = document.querySelectorAll(
+    '.invest-cart__pagination .bullet'
+  );
+  const allInvestCart = document.querySelectorAll('.invest-cart');
+  let clicedBulletIndex = 0;
+
+  allBullets.forEach((bullet, index) => {
+    if (bullet == bulletTarget) {
+      clicedBulletIndex = index;
+      bullet.classList.add('active');
+    } else {
+      bullet.classList.remove('active');
+    }
+  });
+
+  allInvestCart.forEach((cart, index) => {
+    if (index == clicedBulletIndex) {
+      cart.classList.add('active');
+    } else {
+      cart.classList.remove('active');
+    }
+  });
+
+  // stop recursion
+  if (window.ivestToggler) {
+    clearTimeout(window.ivestToggler);
+  }
+}
+
+/**
+ * This function just toggle next .invest-cart
+ * @param {number} parentCount - just counter for invinite rekursion
+ * @return {void}
+ */
+function autoTogglerForIvestCarts(parentCount) {
+  const allBullets = document.querySelectorAll(
+    '.invest-cart__pagination .bullet'
+  );
+  const maxCount = allBullets.length - 1;
+  let count = parentCount || 0;
+
+  window.ivestToggler = setTimeout(() => {
+    if (count > maxCount) {
+      count = 0;
+    }
+    // console.log('I alive');
+    // console.log(allBullets[count]);
+    allBullets[count].click();
+    count++;
+
+    autoTogglerForIvestCarts(count);
+
+    // autostop recursion
+    if (window.ivestToggler > 1000) {
+      clearTimeout(window.ivestToggler);
+    }
+  }, 5000);
 }
